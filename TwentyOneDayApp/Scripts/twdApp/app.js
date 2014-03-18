7aadbc6f6cc4c562b8c402b7f9ccbf7e24f241d1
Sam Winter <sam.winter@gmail.com>
2014-03-17 22:12:29 -0400

    commit 1

---
+++ TwentyOneDayApp/Scripts/twdApp/app.js
@@ -0,0 +1,4 @@
+﻿"use strict";
+
+var twdApp = angular.module("twdApp", []);
+
---
+++ TwentyOneDayApp/Scripts/twdApp/homeController.js
@@ -0,0 +1,50 @@
+﻿"use strict";
+
+twdApp.controller("Home", 
+    function ($scope) {
+        $scope.today = new Date();
+        // the last container that was pressed.
+        $scope.lastPressed = null;
+
+        $scope.greens = [
+            { checked: false, food: "" },
+            { checked: false, food: "" },
+            { checked: false, food: "" },
+            { checked: false, food: "" },
+            { checked: false, food: "" },
+            { checked: false, food: "" }
+        ];
+        $scope.purples = [
+            { checked: false, food: "" },
+            { checked: false, food: "" },
+            { checked: false, food: "" },
+            { checked: false, food: "" }
+        ];
+        $scope.reds = [
+            { checked: false, food: "" },
+            { checked: false, food: "" },
+            { checked: false, food: "" },
+            { checked: false, food: "" },
+            { checked: false, food: "" },
+            { checked: false, food: "" }
+        ];
+        $scope.yellows = [
+            { checked: false, food: "" },
+            { checked: false, food: "" },
+            { checked: false, food: "" },
+            { checked: false, food: "" }
+        ];
+        $scope.blues = [
+            { checked: false, food: "" }
+        ];
+        $scope.oranges = [
+            { checked: false, food: "" }
+        ];
+
+        $scope.pressed = function() {
+            // when pressed, the container should be checked off for the day.
+            this.row.checked = !this.row.checked;
+        };
+    }
+);
+
